e347ab80639cc5b55cca8b471db805425b7303b6
kubix <jakub.gola2@teb.edu.pl>
2024-11-27 17:52:01 +0100

    commit domowy

---
+++ Table.py
@@ -0,0 +1,21 @@
+import random
+
+class Table:
+    def __init__(self, size=50, min=1, max=100):
+        self.size = size
+        self.min = min
+        self.max = max
+        self.n = []
+        self.Fill()
+
+    def Fill(self):
+        self.n = [random.randint(self.min, self.max) for i in range(self.size)]
+
+    def Search(self, value):
+        for i, element in enumerate(self.n):
+            if element == value:
+                return i
+        return -1
+
+    def get_array(self):
+        return self.n
--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-import random
+from Table import Table
 
-def fill_array(size=50, min_value=1, max_value=100):
-    return [random.randint(min_value, max_value) for _ in range(size)]
+class Main:
+    def __init__(self):
+        self.table = Table()
 
+    def main(self):
+        try:
+            value_to_find = int(input("Podaj liczbę całkowitą do wyszukania w tablicy: "))
+        except ValueError:
+            print("Wprowadzono nieprawidłową wartość. Uruchom program ponownie.")
+            return
 
-def search_element(array, value):
-    for index, element in enumerate(array):
-        if element == value:
-            return index
-    return -1
+        index = self.table.Search(value_to_find)
 
+        print("Zawartość tablicy:")
+        print(", ".join(map(str, self.table.get_array())))
 
-def main():
-    array = fill_array()
-    print("Wypełniono tablice losowymi liczbami.")
-
-    try:
-        value_to_find = int(input("Podaj liczbę (całkowitą) do wyszukania w tablicy: "))
-    except ValueError:
-        print("Wprowadzono nieprawidłową wartość. Uruchom program ponownie.")
-        return
-
-    index = search_element(array, value_to_find)
-
-    print("Zawartość tablicy:")
-    print(", ".join(map(str, array)))
-    if index != -1:
-        print(f"\nLiczba {value_to_find} została znaleziona na indeksie: {index}")
-    else:
-        print(f"\nLiczba {value_to_find} nie została znaleziona w tablicy.")
+        if index != -1:
+            print(f"Liczba {value_to_find} została znaleziona na indeksie: {index}")
+        else:
+            print(f"Liczba {value_to_find} nie została znaleziona w tablicy.")
 
 
 if __name__ == "__main__":
-    main()
+    main_program = Main()
+    main_program.main()
